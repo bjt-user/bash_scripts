c767121bf51e591a2ed74a8cf2bcc1776fc87a0c
bjt-user <65905537+bjt-user@users.noreply.github.com>
2021-10-15 19:21:20 +0200

    file is now first command line argument

--- semicolon_searcher.sh
+++ semicolon_searcher.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #this script prints the number of semicolons in each row of the file test.csv
-#todo: use a command line argument as file
 #todo: look up number of rows first, then set loop accordingly
  
-for line in {1..10}
+FILE=$1
+
+for LINE in {1..10}
 do
-  sed -n "${line},${line}p" test.csv | tr -cd ';' | wc -c
+  sed -n "${LINE},${LINE}p" ${FILE} | tr -cd ';' | wc -c
 done
